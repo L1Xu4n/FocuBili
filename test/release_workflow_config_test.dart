80190fa4e81b76f9cbc7869b69ca67d63ee35f8f
L1Xu4n <106089276+L1Xu4n@users.noreply.github.com>
2026-08-16 13:52:08 +0800

    style: format release workflow test

--- test/release_workflow_config_test.dart
+++ test/release_workflow_config_test.dart
@@ -29,10 +29,7 @@ void main() {
     expect(workflow, contains('aapt" dump badging'));
     expect(workflow, contains('apksigner" verify --verbose --print-certs'));
     expect(workflow, contains('Number of signers:'));
-    expect(
-      workflow,
-      contains(r'^Signer #[0-9]+ certificate SHA-256 digest:'),
-    );
+    expect(workflow, contains(r'^Signer #[0-9]+ certificate SHA-256 digest:'));
     expect(workflow, contains(r'V[0-9.]+ Signer'));
     expect(workflow, contains('sort -u'));
     expect(workflow, contains(r'test "$cert_sha256" ='));
